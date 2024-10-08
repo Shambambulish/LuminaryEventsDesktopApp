cbbbd733e8fd5437fd949fbf4175213a817f54f4
t1rima00 <markusveikka@gmail.com>
2024-10-08 13:40:38 +0300

    Crude navbar added

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,24 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import { CssBaseline } from '@mui/material';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/pages/home';
+import { Testisivu } from './components/pages/testisivu';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
+
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
+        <CssBaseline/>
+          <Navbar/>
+          <div>
+            <Routes>
+              <Route path ="/" element={<Home/>}/>
+              <Route path ="/Testisivu" element={<Testisivu/>}/>
+            </Routes>
+          </div>
     </Router>
   );
 }
---
+++ src/renderer/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import { AppBar, Drawer, IconButton, List, ListItem, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+    
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
+    const toggleDrawer = (open: boolean) => {
+        setDrawerOpen(open)
+    }
+
+    const menuItems = (
+        <>
+            <NavLink to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <IconButton>
+                    <Typography>Kotisivu</Typography>
+                </IconButton>
+            </NavLink>
+
+            <NavLink to="Testisivu" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <IconButton>
+                    <Typography>Testisivu</Typography>
+                </IconButton>
+            </NavLink>
+        </>
+        
+    )
+
+    return (
+
+        <>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography>
+                        Electron Testinavbar
+                    </Typography>
+                    {isMobile ? (
+                        <>
+                            <IconButton onClick={() => toggleDrawer(true)}>
+                            </IconButton>
+                            <Drawer open={drawerOpen} onClick={() => toggleDrawer(false)}>
+                                <List>
+                                    <ListItem>{menuItems}</ListItem>
+                                </List>
+                            </Drawer>
+                        </>
+                    ): menuItems}
+                </Toolbar>
+            </AppBar>
+        </>
+        
+    );
+}
