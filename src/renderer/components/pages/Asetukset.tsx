11d9aefee89877192a434d5ca5489c43681d697a
t1rima00 <markusveikka@gmail.com>
2024-10-10 14:14:00 +0300

    Added crude popup for settings if its needed

--- src/renderer/components/pages/Asetukset.tsx
+++ src/renderer/components/pages/Asetukset.tsx
@@ -1,29 +1,51 @@
-import { Checkbox } from "@mui/material";
-import * as React from 'react';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import React from 'react';
+import { Checkbox, FormControlLabel, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import "../css/Asetukset.css"
 
-
 export function Asetukset() {
-
   const [checked, setChecked] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);  
-  }
+    setChecked(event.target.checked);
+  };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-      <div>
-        <div className="settingcontainer">
-          <div className="settings">
-            <FormControlLabel label="Asetus 1"
-            value="asetus1" control={<Checkbox checked={checked} 
-            onChange={handleChange} />}></FormControlLabel>
-          </div>
+  return (
+      <div className="settingcontainer">
+        <div className="settings">
+          <FormControlLabel
+            label="Asetus 1"
+            value="asetus1"
+            control={<Checkbox checked={checked} onChange={handleChange} />}
+          />
         </div>
-      </div>
-    );
-  }
-  
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Open Popup
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Asetukset Poopup</DialogTitle>
+        <DialogContent>
+          Tänne voi tulla asetuksia
+          <FormControlLabel
+            label="Asetus 1"
+            value="asetus1"
+            control={<Checkbox checked={checked} onChange={handleChange} />}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
