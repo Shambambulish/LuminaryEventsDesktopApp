d741e12d131e034c1f9e4f709216e84ec79df702
t1rima00 <markusveikka@gmail.com>
2024-10-11 11:36:46 +0300

    Popup is in its own file, can be opened via navbar

---
+++ src/renderer/components/popup.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Checkbox, FormControlLabel, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+
+interface PopupProps {
+    open: boolean;
+    handleClose: () => void;
+  }
+
+export function Popup({ open, handleClose }: PopupProps) {
+
+
+    const [checked, setChecked] = React.useState(true);
+
+    
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+
+
+return (
+
+<Dialog open={open} onClose={handleClose}>
+<DialogTitle>Asetukset Poopup</DialogTitle>
+<DialogContent>
+  Tänne voi tulla asetuksia
+  <FormControlLabel
+    label="Asetus 1"
+    value="asetus1"
+    control={<Checkbox checked={checked} onChange={handleChange} />}
+  />
+</DialogContent>
+<DialogActions>
+  <Button onClick={handleClose} color="primary">
+    Close
+  </Button>
+</DialogActions>
+</Dialog>
+
+);
+
+}
