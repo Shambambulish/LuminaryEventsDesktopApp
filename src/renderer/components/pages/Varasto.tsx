9facecf113ef2b556855d81dd38098df72430a57
t1rima00 <markusveikka@gmail.com>
2024-10-15 13:26:51 +0300

    Added crude storage front and css

--- src/renderer/components/pages/Varasto.tsx
+++ src/renderer/components/pages/Varasto.tsx
@@ -1,7 +1,92 @@
+import React from 'react';
+import { Button, Box, ThemeProvider, Typography, } from '@mui/material';
+import { luminary } from '../theme';
+import "../css/Varasto.css"
+
 export function Varasto() {
+
+  const handleClick = (boxNumber: number) => {
+    console.log(`Box ${boxNumber} clicked!`);
+  };
+
+
+
   return (
     <div>
-      <h2>Tää o varasto</h2>
+    <div className='cardholder'>
+    <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(1)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Kaikki tuotteet </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+    <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(2)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Tuotteet varastossa </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+    <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(3)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Tuotteet tapahtumissa </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+    </div>
+      <div className='cardholder'>
+      <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(4)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Historia </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+    <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(5)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Skannaa QR-koodi </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+    <div className='card'>
+        <ThemeProvider theme={luminary}>
+        <Box className='box'
+          sx={{
+            backgroundColor: luminary.palette.primary.main,
+          }} onClick={() => handleClick(6)}
+        >
+         <Typography color={luminary.palette.secondaryContrastText} sx={{ pointerEvents: 'none' }}> Luo tuote </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
+
+
+
+
+      </div>
     </div>
   );
 }
