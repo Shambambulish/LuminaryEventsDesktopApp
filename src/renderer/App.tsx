5333e274c8afd9af62aaac4e540bca5cb7a60102
Tino Roivanen <tino.roivanen98@gmail.com>
2024-10-15 12:39:49 +0300

    moved all css files to be in the same folder + refactored paths in tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
-import './App.css';
+import './components/css/App.css';
 import { CssBaseline } from '@mui/material';
 import { Navbar } from './components/Navbar';
 import { Home } from './components/pages/Home';
