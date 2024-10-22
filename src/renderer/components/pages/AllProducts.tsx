d0cf22a65dc58efabc4453ce4ba8909f93b61548
t1rima00 <markusveikka@gmail.com>
2024-10-22 13:27:41 +0300

    Added the option for more detail to the info in All products page

--- src/renderer/components/pages/AllProducts.tsx
+++ src/renderer/components/pages/AllProducts.tsx
@@ -6,28 +6,31 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
-import * as React from 'react';
 import "../css/AllProducts.css"
+import React, { useState } from 'react';
 
 export function AllProducts() {
+
   const navigate = useNavigate();
+
   const [menuValues, setMenuValues] = React.useState({
     menu1: '',
     menu2: '',
     menu3: ''
   });
 
-  const handleClick = (path: string) => {
-    navigate(path);
-  };
-
-  const handleChange = (event: SelectChangeEvent) => {
+  const handleChange = (event: SelectChangeEvent<string>) => {
     const { name, value } = event.target;
     setMenuValues({
       ...menuValues,
       [name]: value
     });
   };
+  
+  const handleClick = (path: string) => {
+    navigate(path);
+  };
+
     return (
       <div>
         <div className='returnbutton'>
@@ -44,6 +47,7 @@ export function AllProducts() {
       <InputLabel id="menu1-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
         <Select
           name="menu1"
+          value={menuValues.menu1}
           onChange={handleChange}
           sx={{ color: 'white' }}
         >
@@ -55,12 +59,28 @@ export function AllProducts() {
           <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
         </Select>
       </FormControl>
+      {menuValues.menu1 === 'valot' && (
+        <div>
+          <p className='detail'>Täällä on tietoa valoista</p>
+        </div>
+      )}
+      {menuValues.menu1 === 'kaiuttimet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kaiuttimista</p>
+        </div>
+      )}
+      {menuValues.menu1 === 'kuulokkeet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kuulokkeista</p>
+        </div>
+        )}
       </div>
       <div>
       <FormControl sx={{ m: 3, minWidth: 350 }}>
-      <InputLabel id="menu1-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
+      <InputLabel id="menu2-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
         <Select
           name="menu2"
+          value={menuValues.menu2}
           onChange={handleChange}
           sx={{ color: 'white' }}
         >
@@ -72,12 +92,28 @@ export function AllProducts() {
           <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
         </Select>
       </FormControl>
+      {menuValues.menu2 === 'valot' && (
+        <div>
+          <p className='detail'>Täällä on tietoa valoista</p>
+        </div>
+      )}
+      {menuValues.menu2 === 'kaiuttimet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kaiuttimista</p>
+        </div>
+      )}
+      {menuValues.menu2 === 'kuulokkeet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kuulokkeista</p>
+        </div>
+        )}
       </div>
       <div>
       <FormControl sx={{ m: 3, minWidth: 350 }}>
-      <InputLabel id="menu1-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
+      <InputLabel id="menu3-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
         <Select
           name="menu3"
+          value={menuValues.menu3}
           onChange={handleChange}
           sx={{ color: 'white' }}
         >
@@ -89,6 +125,21 @@ export function AllProducts() {
           <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
         </Select>
       </FormControl>
+      {menuValues.menu3 === 'valot' && (
+        <div>
+          <p className='detail'>Täällä on tietoa valoista</p>
+        </div>
+      )}
+      {menuValues.menu3 === 'kaiuttimet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kaiuttimista</p>
+        </div>
+      )}
+      {menuValues.menu3 === 'kuulokkeet' && (
+        <div>
+          <p className='detail'>Täällä on tietoa kuulokkeista</p>
+        </div>
+        )}
       </div>
       </div>
     </div>
