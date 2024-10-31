43468b7d657c61597a98e4619d7f4b9fcce46daf
t1rima00 <markusveikka@gmail.com>
2024-10-31 12:32:16 +0200

    InventoryProducts has small working API test

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,5 +2,6 @@ interface Window {
   env: {
     REACT_APP_API_URL: string;
     ELECTRON_APP_SECRET_KEY: string;
+    CONNECTION: string;
   };
 }
--- src/renderer/components/pages/InventoryProducts.tsx
+++ src/renderer/components/pages/InventoryProducts.tsx
@@ -1,9 +1,32 @@
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import { IconButton, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { IconButton, Typography } from '@mui/material';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import axios from 'axios';
+import '../css/InventoryProducts.css';
 
-export function InventoryProducts() {
+interface Device {
+  name: string;
+}
+
+export const InventoryProducts: React.FC = () => {
+  const [device, setDevice] = useState<Device[]>([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("test")/*DONT PUSH INTO GITHUB WITH LINK*/
+      .then(response => {
+        console.log('Data received:', response.data);
+        if (response.data && Array.isArray(response.data)) {
+          setDevice(response.data);
+        } else {
+          console.error('Unexpected response structure:', response.data);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   const handleClick = (path: string) => {
     navigate(path);
@@ -19,7 +42,14 @@ export function InventoryProducts() {
       </div>
       <div>
         <h2>Täällä on varaston tuotteet</h2>
+        <ul className='tekstii'>
+          {device.map((device: Device, index: number) => (
+            <li key={index}>{device.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
-}
+};
+
+export default InventoryProducts;
