50fa790b54aba5ea908d4d247384115c4fe8cb9c
Tino Roivanen <tino.roivanen98@gmail.com>
2024-10-31 11:43:52 +0200

    added axios to package list + prettier

--- src/renderer/components/APIconn.tsx
+++ src/renderer/components/APIconn.tsx
@@ -3,47 +3,48 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 export interface orders {
-    id: number
-    price: number
-    orderLengthDays: number
-    paymentResolved: number
-    orderCreatedAt: string
-    orderStartDate: string
-    orderEndDate: string
-    orderDue: string
-    customerName: string
-    customerPhone: string
-    customerEmail: string
-    orderStatus: string
-    message: string
+  id: number;
+  price: number;
+  orderLengthDays: number;
+  paymentResolved: number;
+  orderCreatedAt: string;
+  orderStartDate: string;
+  orderEndDate: string;
+  orderDue: string;
+  customerName: string;
+  customerPhone: string;
+  customerEmail: string;
+  orderStatus: string;
+  message: string;
 }
 
 function APIconn() {
-    const [data, setData] = useState<orders[]>([]);
+  const [data, setData] = useState<orders[]>([]);
 
-    useEffect (() => {
-        axios.get('LAITA OMA URLI TÄHÄN')
-            .then(response => {
-                console.log(response.data);
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching data: ', error.response.data);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(window.env.REACT_APP_API_URL)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error.response.data);
+      });
+  }, []);
 
-    return (
-        <Box className="calendarinfo">
-                {data.map(item => {
-                    return(
-                        <div className="eventinfo">
-                            <p> {item.orderStartDate} </p>
-                            <p> {item.customerName} </p>
-                            <p> {item.message} </p>
-                        </div>
-                    );
-                })}
-        </Box>
-    );
+  return (
+    <Box className="calendarinfo">
+      {data.map((item) => {
+        return (
+          <div className="eventinfo">
+            <p> {item.orderStartDate} </p>
+            <p> {item.customerName} </p>
+            <p> {item.message} </p>
+          </div>
+        );
+      })}
+    </Box>
+  );
 }
 export default APIconn;
--- src/renderer/components/pages/AllProducts.tsx
+++ src/renderer/components/pages/AllProducts.tsx
@@ -6,141 +6,147 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
-import "../css/AllProducts.css"
+import '../css/AllProducts.css';
 import React, { useState } from 'react';
 
 export function AllProducts() {
-
   const navigate = useNavigate();
 
   const [menuValues, setMenuValues] = React.useState({
     menu1: '',
     menu2: '',
-    menu3: ''
+    menu3: '',
   });
 
   const handleChange = (event: SelectChangeEvent<string>) => {
     const { name, value } = event.target;
     setMenuValues({
       ...menuValues,
-      [name]: value
+      [name]: value,
     });
   };
-  
+
   const handleClick = (path: string) => {
     navigate(path);
   };
 
-    return (
-      <div>
-        <div className='returnbutton'>
-      <IconButton onClick={() => handleClick('/Inventory')}>
-        <Typography className='returntext'> Palaa</Typography><KeyboardReturnIcon></KeyboardReturnIcon>
-      </IconButton>
+  return (
+    <div>
+      <div className="returnbutton">
+        <IconButton onClick={() => handleClick('/Inventory')}>
+          <Typography className="returntext"> Palaa</Typography>
+          <KeyboardReturnIcon></KeyboardReturnIcon>
+        </IconButton>
       </div>
-      <div className='storage'>
+      <div className="storage">
         <div>
-          <Typography className='storageheader'>Kaikki tuotteet</Typography>
+          <Typography className="storageheader">Kaikki tuotteet</Typography>
         </div>
         <div>
-      <FormControl sx={{ m: 3, minWidth: 350 }}>
-      <InputLabel id="menu1-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
-        <Select
-          name="menu1"
-          value={menuValues.menu1}
-          onChange={handleChange}
-          sx={{ color: 'white' }}
-        >
-          <MenuItem value="">
-            <em>Tyhjä</em>
-          </MenuItem>
-          <MenuItem value="valot">Valot</MenuItem>
-          <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
-          <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
-        </Select>
-      </FormControl>
-      {menuValues.menu1 === 'valot' && (
-        <div>
-          <p className='detail'>Täällä on tietoa valoista</p>
+          <FormControl sx={{ m: 3, minWidth: 350 }}>
+            <InputLabel id="menu1-label" sx={{ color: 'white' }}>
+              Valitse tuote
+            </InputLabel>
+            <Select
+              name="menu1"
+              value={menuValues.menu1}
+              onChange={handleChange}
+              sx={{ color: 'white' }}
+            >
+              <MenuItem value="">
+                <em>Tyhjä</em>
+              </MenuItem>
+              <MenuItem value="valot">Valot</MenuItem>
+              <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
+              <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
+            </Select>
+          </FormControl>
+          {menuValues.menu1 === 'valot' && (
+            <div>
+              <p className="detail">Täällä on tietoa valoista</p>
+            </div>
+          )}
+          {menuValues.menu1 === 'kaiuttimet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kaiuttimista</p>
+            </div>
+          )}
+          {menuValues.menu1 === 'kuulokkeet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kuulokkeista</p>
+            </div>
+          )}
         </div>
-      )}
-      {menuValues.menu1 === 'kaiuttimet' && (
         <div>
-          <p className='detail'>Täällä on tietoa kaiuttimista</p>
+          <FormControl sx={{ m: 3, minWidth: 350 }}>
+            <InputLabel id="menu2-label" sx={{ color: 'white' }}>
+              Valitse tuote
+            </InputLabel>
+            <Select
+              name="menu2"
+              value={menuValues.menu2}
+              onChange={handleChange}
+              sx={{ color: 'white' }}
+            >
+              <MenuItem value="">
+                <em>Tyhjä</em>
+              </MenuItem>
+              <MenuItem value="valot">Valot</MenuItem>
+              <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
+              <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
+            </Select>
+          </FormControl>
+          {menuValues.menu2 === 'valot' && (
+            <div>
+              <p className="detail">Täällä on tietoa valoista</p>
+            </div>
+          )}
+          {menuValues.menu2 === 'kaiuttimet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kaiuttimista</p>
+            </div>
+          )}
+          {menuValues.menu2 === 'kuulokkeet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kuulokkeista</p>
+            </div>
+          )}
         </div>
-      )}
-      {menuValues.menu1 === 'kuulokkeet' && (
         <div>
-          <p className='detail'>Täällä on tietoa kuulokkeista</p>
+          <FormControl sx={{ m: 3, minWidth: 350 }}>
+            <InputLabel id="menu3-label" sx={{ color: 'white' }}>
+              Valitse tuote
+            </InputLabel>
+            <Select
+              name="menu3"
+              value={menuValues.menu3}
+              onChange={handleChange}
+              sx={{ color: 'white' }}
+            >
+              <MenuItem value="">
+                <em>Tyhjä</em>
+              </MenuItem>
+              <MenuItem value="valot">Valot</MenuItem>
+              <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
+              <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
+            </Select>
+          </FormControl>
+          {menuValues.menu3 === 'valot' && (
+            <div>
+              <p className="detail">Täällä on tietoa valoista</p>
+            </div>
+          )}
+          {menuValues.menu3 === 'kaiuttimet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kaiuttimista</p>
+            </div>
+          )}
+          {menuValues.menu3 === 'kuulokkeet' && (
+            <div>
+              <p className="detail">Täällä on tietoa kuulokkeista</p>
+            </div>
+          )}
         </div>
-        )}
-      </div>
-      <div>
-      <FormControl sx={{ m: 3, minWidth: 350 }}>
-      <InputLabel id="menu2-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
-        <Select
-          name="menu2"
-          value={menuValues.menu2}
-          onChange={handleChange}
-          sx={{ color: 'white' }}
-        >
-          <MenuItem value="">
-            <em>Tyhjä</em>
-          </MenuItem>
-          <MenuItem value="valot">Valot</MenuItem>
-          <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
-          <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
-        </Select>
-      </FormControl>
-      {menuValues.menu2 === 'valot' && (
-        <div>
-          <p className='detail'>Täällä on tietoa valoista</p>
-        </div>
-      )}
-      {menuValues.menu2 === 'kaiuttimet' && (
-        <div>
-          <p className='detail'>Täällä on tietoa kaiuttimista</p>
-        </div>
-      )}
-      {menuValues.menu2 === 'kuulokkeet' && (
-        <div>
-          <p className='detail'>Täällä on tietoa kuulokkeista</p>
-        </div>
-        )}
-      </div>
-      <div>
-      <FormControl sx={{ m: 3, minWidth: 350 }}>
-      <InputLabel id="menu3-label" sx={{ color: 'white' }}>Valitse tuote</InputLabel>
-        <Select
-          name="menu3"
-          value={menuValues.menu3}
-          onChange={handleChange}
-          sx={{ color: 'white' }}
-        >
-          <MenuItem value="">
-            <em>Tyhjä</em>
-          </MenuItem>
-          <MenuItem value="valot">Valot</MenuItem>
-          <MenuItem value="kaiuttimet">Kaiuttimet</MenuItem>
-          <MenuItem value="kuulokkeet">Kuulokkeet</MenuItem>
-        </Select>
-      </FormControl>
-      {menuValues.menu3 === 'valot' && (
-        <div>
-          <p className='detail'>Täällä on tietoa valoista</p>
-        </div>
-      )}
-      {menuValues.menu3 === 'kaiuttimet' && (
-        <div>
-          <p className='detail'>Täällä on tietoa kaiuttimista</p>
-        </div>
-      )}
-      {menuValues.menu3 === 'kuulokkeet' && (
-        <div>
-          <p className='detail'>Täällä on tietoa kuulokkeista</p>
-        </div>
-        )}
-      </div>
       </div>
     </div>
   );
