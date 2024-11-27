664c02e09b786d747fa452bcf5c95817f4a9704d
onde <onni.loponen@gmail.com>
2024-11-27 14:52:24 +0200

    H

--- src/renderer/components/pages/History.tsx
+++ src/renderer/components/pages/History.tsx
@@ -76,7 +76,7 @@ export function History() {
                   <ListItemButton>
                     Muutosaika:
                     <br />
-                    {dayjs(history.time_changed).format('LLL')}
+                    {dayjs(History.time_changed).format('LLL')}
                   </ListItemButton>
                 </ListItem>
               </List>
