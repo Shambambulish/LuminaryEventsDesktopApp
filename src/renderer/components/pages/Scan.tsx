44807d5315d34160dfe07c0c058a5fa8b2cc4502
t1rima00 <markusveikka@gmail.com>
2024-12-13 13:49:16 +0200

    added crude PDF print to scan file

--- src/renderer/components/pages/Scan.tsx
+++ src/renderer/components/pages/Scan.tsx
@@ -1,24 +1,47 @@
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import { IconButton, Typography } from '@mui/material';
+import { IconButton, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import '../css/Scan.css';
 
 export function Scan() {
   const navigate = useNavigate();
+  const componentRef = useRef<HTMLDivElement>(null);
+
+  const handlePrint = () => {
+    if (componentRef.current) {
+      const printContents = componentRef.current.innerHTML;
+      const originalContents = document.body.innerHTML;
+
+      document.body.innerHTML = printContents;
+      window.print();
+      document.body.innerHTML = originalContents;
+      window.location.reload();
+    }
+  };
 
   const handleClick = (path: string) => {
     navigate(path);
   };
 
   return (
-    <div>
+    <div ref={componentRef}>
       <div className="returnbutton">
         <IconButton onClick={() => handleClick('/Inventory')}>
           <Typography className="returntext"> Palaa</Typography>
-          <KeyboardReturnIcon></KeyboardReturnIcon>
+          <KeyboardReturnIcon />
         </IconButton>
       </div>
       <div>
-        <h2>Täällä voi skannata QR koodeja</h2>
+        <h2>Täällä ei voi skannata QR koodeja. Täällä paistetaan räiskäleitä ja tulostetaan PDF-tiedostoja.</h2>
+        <div>
+          <Button variant="contained" color="primary" onClick={handlePrint}>
+            Print
+          </Button>
+          <div className="printable">
+            <Typography> REEEE </Typography>
+          </div>
+        </div>
       </div>
     </div>
   );
