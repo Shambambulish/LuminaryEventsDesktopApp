7e1e95286de16dc76540574f43087395126988ee
pahvone <ville.pelimanni@gmail.com>
2024-12-13 13:35:06 +0200

    DB export added

--- src/renderer/components/pages/Settings.tsx
+++ src/renderer/components/pages/Settings.tsx
@@ -1,60 +1,119 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import {
   Button,
-  Checkbox,
-  FormControlLabel,
   ThemeProvider,
   Typography,
-  useTheme,
+  ButtonGroup,
 } from '@mui/material';
 import '../css/Settings.css';
 import { luminary } from '../Theme';
+import { _get } from '../APIconn';
+import exportFromJSON, {ExportType} from 'export-from-json';
 
 export function Settings() {
-  const [checked, setChecked] = React.useState(true);
+  const [exportType, setExportType] = useState<ExportType>();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
+  const handleDeviceExport = async () => {
+    try {
+      const devicedata = await fetchDevices();
+      exportFromJSON({ data: devicedata, fileName: 'devices', exportType });
+    } catch (error) {
+      console.error('Error exporting devices:', error);
+    }
   };
+  const handleOrderExport = async () => {
+    try {
+      const orderdata = await fetchOrders();
+      exportFromJSON({ data: orderdata, fileName: 'orders', exportType });
+    } catch (error) {
+      console.error('Error exporting orders:', error);
+    }
+  };
+  const handleCustomExport = async () => {
+    try {
+      const customdata = await fetchCustom();
+      exportFromJSON({ data: customdata, fileName: 'custom', exportType });
+    } catch (error) {
+      console.error('Error exporting:', error);
+    }
+  };
+  
+  const fetchDevices = async () => {
+    try {
+      const response = await _get(`devices`);
+      const devicedata = response.data;
+      return devicedata;
+
+    } catch (error) {
+      console.error('Error fetching device data:', error);
+    }
+  };
+  const fetchOrders = async () => {
+    try {
+      const response = await _get(`orders`);
+      const orderdata = response.data;
+      return orderdata;
+    } catch (error) {
+      console.error('Error fetching order data:', error);
+    }
+  };
+  /*const fetchCustom = async () => {
+    try {
+      const response = await _get({url: `custom`});
+      const customdata = response.data;
+      console.log("Fetched data: ", data);
+      return customdata;
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };*/
+
+  useEffect(() => {
+    fetchDevices();
+    fetchOrders();
+   // fetchCustom();
+  }, []);
 
   return (
     <div className="settingcontainer">
-      <div className="settings">
-        <FormControlLabel
-          label="Asetus 1"
-          value="asetus1"
-          control={<Checkbox checked={checked} onChange={handleChange} />}
-        />
-      </div>
       <ThemeProvider theme={luminary}>
+        <ButtonGroup variant="contained" aria-label="Basic button group">
+          <Button className={`nabbi ${exportType === 'json' ? 'focus' : ''}`} onClick={() => setExportType('json')}>JSON</Button>
+          <Button className={`nabbi ${exportType === 'xls' ? 'focus' : ''}`} onClick={() => setExportType('xls')}>XLS</Button>
+          <Button className={`nabbi ${exportType === 'csv' ? 'focus' : ''}`} onClick={() => setExportType('csv')}>CSV</Button>
+        </ButtonGroup>
         <Button
           variant="contained"
+          onClick={handleDeviceExport}
           style={{
             backgroundColor: luminary.palette.primary.dark,
           }}
         >
-          Asetus 1
+          Backup Devices
         </Button>
         <Button
           variant="contained"
+          onClick={handleOrderExport}
           sx={{
             backgroundColor: luminary.palette.primary.main,
           }}
         >
           <Typography color={luminary.palette.secondaryContrastText}>
             {' '}
-            Asetus 2{' '}
+            Backup Orders{' '}
           </Typography>
         </Button>
         <Button
           variant="contained"
+          onClick={handleCustomExport}
           sx={{
             backgroundColor: luminary.palette.lumBlue,
           }}
         >
           <Typography color={luminary.palette.secondaryContrastText}>
             {' '}
-            Asetus 3{' '}
+            Backup Other{' '}
           </Typography>
         </Button>
       </ThemeProvider>
